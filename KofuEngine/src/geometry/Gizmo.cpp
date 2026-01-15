a5ac55652eea4be8cafc44dfc97c94957cc413af
munucrafts <sarthakmishra520@gmail.com>
2026-01-15 18:41:38 +0530

    Top Right Corner World Gizmo Mesh Updated

--- KofuEngine/src/geometry/Gizmo.cpp
+++ KofuEngine/src/geometry/Gizmo.cpp
@@ -6,10 +6,11 @@ void Gizmo::Init()
 {
 	MeshData meshData;
 	meshData.meshType = STATIC_MESH;
-	gizmoMesh = GLTFLoader::GetGltfLoader().LoadGltfModel("./assets/models/Gizmo/scene.gltf", meshData)[0];
+	gizmoMesh = GLTFLoader::GetGltfLoader().LoadGltfModel("./assets/models/Gizmo/gizmo.gltf", meshData)[0];
 	gizmoMesh->InitMesh();
 
 	StaticMesh* staticPtr = static_cast<StaticMesh*>(gizmoMesh);
+	staticPtr->transform.scale = glm::vec3(0.025f);
 	glm::mat4& gizmoModel = staticPtr->modelMat;
 	gizmoModel[3] = glm::vec4(0, 0, 0, 1);
 	gizmoModel[0] = glm::normalize(gizmoModel[0]);
