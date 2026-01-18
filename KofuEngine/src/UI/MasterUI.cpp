6c2693602542b674a06b1e23007e38889c7e2b59
munucrafts <sarthakmishra520@gmail.com>
2026-01-18 17:24:06 +0530

    Basic MasterUI and Panel UI System Implemented

--- KofuEngine/src/UI/MasterUI.cpp
+++ KofuEngine/src/UI/MasterUI.cpp
@@ -1,7 +1,10 @@
 #pragma once
 #include "MasterUI.h"
 #include "rendering/Scene.h"
-#include <imgui.h>
+#include <Engine.h>
+#include "imgui.h"                
+#include "backends/imgui_impl_glfw.h"      
+#include "backends/imgui_impl_opengl3.h"   
 
 MasterUI& MasterUI::GetMasterUI()
 {
@@ -13,11 +16,25 @@ void MasterUI::InitMasterUI()
 {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
+
+	Engine& engine = Engine::GetEngine();
+
+	ImGui_ImplGlfw_InitForOpenGL(engine.window, true);
+	ImGui_ImplOpenGL3_Init(engine.GetOGLVersionText().c_str());
 }
 
 void MasterUI::RenderMasterUI(Scene* activeScene)
 {
-	outlinerPanel.RenderUI(activeScene);
-	viewportPanel.RenderUI(activeScene);
-	detailsPanel.RenderUI(activeScene);
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    outlinerPanel.RenderUI(activeScene);
+    viewportPanel.RenderUI(activeScene);
+    detailsPanel.RenderUI(activeScene);
+
+    ImGui::Render();
+
+    ImDrawData* drawData = ImGui::GetDrawData();
+    if (drawData) ImGui_ImplOpenGL3_RenderDrawData(drawData);
 }
--- KofuEngine/vendor/imgui/premake5.lua
+++ KofuEngine/vendor/imgui/premake5.lua
@@ -13,11 +13,24 @@ project "ImGui"
 		"imgui_draw.cpp",
 		"imgui_internal.h",
 		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3.cpp"
 	}
+
+    includedirs
+    {
+        ".",
+        "backends",
+        "../glfw/include" 
+    }
 
 	filter "system:windows"
 		systemversion "latest"
