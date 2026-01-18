f5fa786fa7024bff5a1e84db87b2460627d1436e
munucrafts <sarthakmishra520@gmail.com>
2026-01-18 23:55:20 +0530

    Few Modifications in Scene Entity Buttons

--- KofuEngine/src/UI/OutlinerPanel.cpp
+++ KofuEngine/src/UI/OutlinerPanel.cpp
@@ -5,40 +5,38 @@
 
 void OutlinerPanel::RenderUI(Scene* activeScene)
 {
-	ImGui::Begin("Outliner");
+    ImGui::Begin("Outliner");
 
-	if (ImGui::TreeNode("Meshes"))
-	{
-		for (int i = 0; i < activeScene->meshes.size(); i++)
-		{
-			std::string name = "Mesh " + std::to_string(i);
-			ImGui::Button(name.c_str(), ImVec2(ImVec2(ImGui::GetContentRegionAvail().x, 0.0f)));
-		}
+    ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign, ImVec2(0.0f, 0.5f));
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4.0f, 1.0f));
 
-		ImGui::TreePop();
-	}
+    if (ImGui::TreeNode("Meshes"))
+    {
+        for (int i = 0; i < activeScene->meshes.size(); i++)
+        {
+            std::string name = "Mesh " + std::to_string(i);
+            ImGui::Button(name.c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0.0f));
+        }
+        ImGui::TreePop();
+    }
 
-	if (ImGui::TreeNode("Lights"))
-	{
-		for (int i = 0; i < activeScene->lights.size(); i++)
-		{
-			std::string name = "Light " + std::to_string(i);
-			ImGui::Button(name.c_str(), ImVec2(ImVec2(ImGui::GetContentRegionAvail().x, 0.0f)));
-		}
+    if (ImGui::TreeNode("Lights"))
+    {
+        for (int i = 0; i < activeScene->lights.size(); i++)
+        {
+            std::string name = "Light " + std::to_string(i);
+            ImGui::Button(name.c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0.0f));
+        }
+        ImGui::TreePop();
+    }
 
-		ImGui::TreePop();
-	}
+    if (ImGui::TreeNode("Cameras"))
+    {
+        std::string name = "Camera 0";
+        ImGui::Button(name.c_str(), ImVec2(ImGui::GetContentRegionAvail().x, 0.0f));
+        ImGui::TreePop();
+    }
 
-	if (ImGui::TreeNode("Camera"))
-	{
-		for (int i = 0; i < activeScene->meshes.size(); i++)
-		{
-			std::string name = "Camera " + std::to_string(i);
-			ImGui::Button(name.c_str(), ImVec2(ImVec2(ImGui::GetContentRegionAvail().x, 0.0f)));
-		}
-
-		ImGui::TreePop();
-	}
-
-	ImGui::End();
+    ImGui::PopStyleVar(2);
+    ImGui::End();
 }
